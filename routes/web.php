86793801f32ca6a33ecc78c11547e37400117ccc
Alexander Fellner <alexander.fellner@yahoo.de>
2020-07-27 16:39:00 +0200

    Correct path issue

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.welcome');
 });
 Route::get('/posts','PostController@index');
 Route::post('/store_post','PostController@store')->name('store_post');
